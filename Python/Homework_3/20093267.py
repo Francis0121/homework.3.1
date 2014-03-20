7c8d71f5c1208da56f3c369aab4c763b465cf9d1
Francis Kim <kims_0121@naver.com>
2014-03-20 13:33:10 +0900

    Homework 3번
    - $ grep “스트링” “파일명” -- “파일명”에서 “스트링”이 있는 라인만 출력
    - $ grep “스트링” “폴더명” -- 해당 폴더의 각 *.txt 파일에서 “스트링”이 있는 라인만 출력

--- Python/Homework_3/20093267.py
+++ Python/Homework_3/20093267.py
@@ -1,3 +1,54 @@
 # -*- coding: cp949 -*-
 import sys
+import re
+import glob
+import os
 
+# --- Method ---
+def checkSystemArgument(args):
+    
+    if ((not args) or (args[0] != "grep") or (len(args) != 3) or (not args[1]) or (not args[2])) :
+        print("Please enter command : \n\tgrep 'statement' 'fileName' \n\tgrep 'statement' 'directory'")
+        return 0
+    return 1
+
+def stringMatchFile(fileName, string) :
+    inputFile = open(fileName, 'r')
+
+    while 1:
+        line = inputFile.readline()
+        line.strip()
+        if not line: break
+
+        isPrint = 0
+
+        regExp = "%s" %string
+        if re.search(regExp, line):
+            print(" ".join(line.split())) # 개행문자제거 
+            
+    inputFile.close()
+                
+# --- Main ---
+# 정확한 grep 'statement' 'fileName' format이 들어올때 까지 입력받음
+args = sys.argv[1:]
+
+while 1:
+    if not checkSystemArgument(args):
+        temp = raw_input('')
+        args = temp.split(' ')
+    else:
+        break
+
+string = args[1]
+path = args[2]
+
+if bool(re.search('.*\.txt', path)) :
+    stringMatchFile(path, string) 
+else :
+    path = "%s\*.txt" %path
+    fileNames = glob.glob(path)
+    for fileName in (fileNames):
+        print ("-------------------------------")
+        print ("%s" %os.path.basename(fileName))
+        stringMatchFile(fileName, string)
+        print ("-------------------------------\n")
