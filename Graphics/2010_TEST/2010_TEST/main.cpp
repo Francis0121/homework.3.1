383ae3e177c72378298d58ecd50d4ad0fadd0826
Francis Kim <kims_0121@naver.com>
2014-04-26 14:45:14 +0900

    Graphics 이동 변환이 아닌 클릭시 다른 Object 생성

--- Graphics/2010_TEST/2010_TEST/main.cpp
+++ Graphics/2010_TEST/2010_TEST/main.cpp
@@ -6,6 +6,26 @@ void init();
 void InitGeometry();
 void InitVBO();
 void display();
+void reshape(int w, int h);
+void mouse(int button, int state, int x, int y);
+
+
+typedef struct { 
+	float location[3]; 
+	float color[4]; 
+} Vertex; 
+
+typedef struct {
+	GLubyte *order;
+	int size;
+} Index;
+
+Index indeices[4];
+Vertex verts[16]; // triangle vertices 
+GLuint vboHandle[1]; // a VBO that contains interleaved positions and colors 
+GLuint indexVBO[1]; 
+GLdouble aspect = 3.0f/4.0f;
+int frame_loop = 0;
 
 int main(int argc, char* argv[])
 {
@@ -22,21 +42,24 @@ int main(int argc, char* argv[])
 
 	InitGeometry();
 	InitVBO();
+
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
 	glutDisplayFunc(display);
 
 	glutMainLoop();
+
+	for(int i=0; i<sizeof(indeices);i++)
+		delete indeices[i].order;
+
 	return 0;
 }
 
 void init(){
-	// Clear buffers to preset value
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glEnable(GL_DEPTH_TEST);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	// ~ Intrinsic
 	glMatrixMode(GL_PROJECTION); 
 	glLoadIdentity(); 
-	gluPerspective(90.0, 3.0/4.0, 0.1, 100.0);
+	gluPerspective(90.0, aspect, 0.1, 100.0);
 		
 	// ~ Extrinisic
 	glMatrixMode(GL_MODELVIEW); 
@@ -45,15 +68,6 @@ void init(){
 				0.0f, 0.0f, 0.0f,
 				0.0f, 1.0f, 0.0f);
 }
-
-typedef struct { 
-	float location[3]; 
-	float color[4]; 
-} Vertex; 
-	
-Vertex verts[16]; // triangle vertices 
-GLuint vboHandle[1]; // a VBO that contains interleaved positions and colors 
-GLuint indexVBO; 
 
 void InitGeometry() { 
 	float black = 64.0/255.0f;
@@ -78,61 +92,126 @@ void InitGeometry() {
 	verts[14].location[0] = 0.1f; verts[14].location[1] = -3.8f; verts[14].location[2] = 0.0f; 
 	verts[15].location[0] = 2.8f; verts[15].location[1] = -3.8f; verts[15].location[2] = 0.0f; 
 
-	// [0] = Opacity? [1,2,3] บฮลอ RGB
-	verts[0].color[0] = 1; verts[0].color[1] = black; verts[0].color[2] = black; verts[0].color[3] = black; 
-	verts[1].color[0] = 1; verts[1].color[1] = black; verts[1].color[2] = black; verts[1].color[3] = black; 
-	verts[2].color[0] = 1; verts[2].color[1] = black; verts[2].color[2] = black; verts[2].color[3] = black; 
-	verts[3].color[0] = 1; verts[3].color[1] = black; verts[3].color[2] = black; verts[3].color[3] = black; 
-	verts[4].color[0] = 1; verts[4].color[1] = black; verts[4].color[2] = black; verts[4].color[3] = black; 
-	verts[5].color[0] = 1; verts[5].color[1] = black; verts[5].color[2] = black; verts[5].color[3] = black; 
-	verts[6].color[0] = 1; verts[6].color[1] = black; verts[6].color[2] = black; verts[6].color[3] = black; 
-	verts[7].color[0] = 1; verts[7].color[1] = black; verts[7].color[2] = black; verts[7].color[3] = black; 
-	verts[8].color[0] = 1; verts[8].color[1] = black; verts[8].color[2] = black; verts[8].color[3] = black; 
-	verts[9].color[0] = 1; verts[9].color[1] = black; verts[9].color[2] = black; verts[9].color[3] = black; 
-	verts[10].color[0] = 1; verts[10].color[1] = black; verts[10].color[2] = black; verts[10].color[3] = black; 
-	verts[11].color[0] = 1; verts[11].color[1] = black; verts[11].color[2] = black; verts[11].color[3] = black; 
-	verts[12].color[0] = 1; verts[12].color[1] = black; verts[12].color[2] = black; verts[12].color[3] = black; 
-	verts[13].color[0] = 1; verts[13].color[1] = black; verts[13].color[2] = black; verts[13].color[3] = black; 
-	verts[14].color[0] = 1; verts[14].color[1] = black; verts[14].color[2] = black; verts[14].color[3] = black; 
-	verts[15].color[0] = 1; verts[15].color[1] = black; verts[15].color[2] = black; verts[15].color[3] = black; 
+	// RGBA
+	verts[0].color[0] = black; verts[0].color[1] = black; verts[0].color[2] = black; verts[0].color[3] = 1; 
+	verts[1].color[0] = black; verts[1].color[1] = black; verts[1].color[2] = black; verts[1].color[3] = 1; 
+	verts[2].color[0] = black; verts[2].color[1] = black; verts[2].color[2] = black; verts[2].color[3] = 1; 
+	verts[3].color[0] = black; verts[3].color[1] = black; verts[3].color[2] = black; verts[3].color[3] = 1; 
+	verts[4].color[0] = black; verts[4].color[1] = black; verts[4].color[2] = black; verts[4].color[3] = 1; 
+	verts[5].color[0] = black; verts[5].color[1] = black; verts[5].color[2] = black; verts[5].color[3] = 1; 
+	verts[6].color[0] = black; verts[6].color[1] = black; verts[6].color[2] = black; verts[6].color[3] = 1; 
+	verts[7].color[0] = black; verts[7].color[1] = black; verts[7].color[2] = black; verts[7].color[3] = 1; 
+	verts[8].color[0] = black; verts[8].color[1] = black; verts[8].color[2] = black; verts[8].color[3] = 1; 
+	verts[9].color[0] = black; verts[9].color[1] = black; verts[9].color[2] = black; verts[9].color[3] = 1; 
+	verts[10].color[0] = black; verts[10].color[1] = black; verts[10].color[2] = black; verts[10].color[3] = 1; 
+	verts[11].color[0] = black; verts[11].color[1] = black; verts[11].color[2] = black; verts[11].color[3] = 1; 
+	verts[12].color[0] = black; verts[12].color[1] = black; verts[12].color[2] = black; verts[12].color[3] = 1; 
+	verts[13].color[0] = black; verts[13].color[1] = black; verts[13].color[2] = black; verts[13].color[3] = 1; 
+	verts[14].color[0] = black; verts[14].color[1] = black; verts[14].color[2] = black; verts[14].color[3] = 1; 
+	verts[15].color[0] = black; verts[15].color[1] = black; verts[15].color[2] = black; verts[15].color[3] = 1; 
+	
+	indeices[0].order = new GLubyte[18];
+	indeices[0].size = 18;
+	
+	indeices[0].order[0] = 0; indeices[0].order[1] = 5; indeices[0].order[2] = 6;
+	indeices[0].order[3] = 6; indeices[0].order[4] = 3; indeices[0].order[5] = 0;
+
+	indeices[0].order[6] = 8; indeices[0].order[7] = 9; indeices[0].order[8] = 10;
+	indeices[0].order[9] = 10; indeices[0].order[10] = 11; indeices[0].order[11] = 8;
+
+	indeices[0].order[12] = 12; indeices[0].order[13] = 13; indeices[0].order[14] = 14;
+	indeices[0].order[15] = 14; indeices[0].order[16] = 15; indeices[0].order[17] = 12;
+
+	indeices[1].order = new GLubyte[12];
+	indeices[1].size = 12;
+
+	indeices[1].order[0] = 0; indeices[1].order[1] = 5; indeices[1].order[2] = 6;
+	indeices[1].order[3] = 6; indeices[1].order[4] = 3; indeices[1].order[5] = 0;
+
+	indeices[1].order[6] = 12; indeices[1].order[7] = 9; indeices[1].order[8] = 10;
+	indeices[1].order[9] = 10; indeices[1].order[10] = 15; indeices[1].order[11] = 12;
+
+	indeices[2].order = new GLubyte[18];
+	indeices[2].size = 18;
+
+	indeices[2].order[0] = 4; indeices[2].order[1] = 5; indeices[2].order[2] = 10;
+	indeices[2].order[3] = 10; indeices[2].order[4] = 11; indeices[2].order[5] = 4;
+
+	indeices[2].order[6] = 0; indeices[2].order[7] = 1; indeices[2].order[8] = 2;
+	indeices[2].order[9] = 2; indeices[2].order[10] = 3; indeices[2].order[11] = 0;
+
+	indeices[2].order[12] = 12; indeices[2].order[13] = 13; indeices[2].order[14] = 14;
+	indeices[2].order[15] = 14; indeices[2].order[16] = 15; indeices[2].order[17] = 12;
+
+	indeices[3].order = new GLubyte[12];
+	indeices[3].size = 12;
+
+	indeices[3].order[0] = 4; indeices[3].order[1] = 5; indeices[3].order[2] = 10;
+	indeices[3].order[3] = 10; indeices[3].order[4] = 11; indeices[3].order[5] = 4;
+
+	indeices[3].order[6] = 0; indeices[3].order[7] = 1; indeices[3].order[8] = 14;
+	indeices[3].order[9] = 14; indeices[3].order[10] = 15; indeices[3].order[11] = 0;
 }
 
-void InitVBO() { 
-	GLubyte tindices[] = {	0, 5, 6, 6, 3, 0,
-							8, 9, 10, 10, 11, 8,
-							12, 13, 14, 14, 15, 12 }; // triangle vertex indices 
-
+void InitVBO() {
 	glGenBuffers(1, vboHandle); // create two VBO handles, one position, one color handle 
 	glBindBuffer(GL_ARRAY_BUFFER, vboHandle[0]); // bind the first handle 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)*16, verts, GL_STATIC_DRAW); // allocate space and copy the 
 	// position data over 
 	glBindBuffer(GL_ARRAY_BUFFER, 0); // clean up 
-	glGenBuffers(1, &indexVBO); 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVBO); 
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLubyte)*18, tindices, GL_STATIC_DRAW); // load the 
+	glGenBuffers(1, indexVBO); 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVBO[0]); 
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLubyte)*indeices[frame_loop].size, indeices[frame_loop].order, GL_STATIC_DRAW); // load the 
 	// index data 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0); // clean up 
-	// by now, we moved the position and color data over to the graphics card. There will be no redundant data 
-	// copy at drawing time 
 }
 
-void display() 
-{ 
+void display() { 
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
 	glBindBuffer(GL_ARRAY_BUFFER, vboHandle[0]); 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVBO); 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVBO[0]); 
 	
 	glEnableClientState(GL_VERTEX_ARRAY); // enable the vertex array on the client side 
 	glEnableClientState(GL_COLOR_ARRAY); // enable the color array on the client side 
 	
 	// number of coordinates per vertex (4 here), type of the coordinates, 
 	// stride between consecutive vertices, and pointers to the first coordinate 
-	glColorPointer(4, GL_FLOAT, sizeof(Vertex), (char*) NULL+ 16); 
+	glColorPointer(4, GL_FLOAT, sizeof(Vertex), (char*) NULL+ 12); 
 	glVertexPointer(3,GL_FLOAT, sizeof(Vertex), (char*) NULL+ 0); 
 	
-	glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_BYTE, (char*) NULL+0); 
+	glDrawElements(GL_TRIANGLES, indeices[frame_loop].size, GL_UNSIGNED_BYTE, (char*) NULL+0); 
 	
 	glDisableClientState(GL_VERTEX_ARRAY); 
 	glDisableClientState(GL_COLOR_ARRAY); 
 	
 	glFlush(); 
+}
+
+void reshape(int w, int h){
+	glViewport(0, 0, w, h);
+	glutPostRedisplay(); 
+}
+
+void mouse(int button, int state, int x, int y){
+	if(button == GLUT_LEFT_BUTTON && state==GLUT_DOWN){
+		frame_loop+=1;
+		
+		std::cout << frame_loop << std::endl;
+
+		if(frame_loop == 4){
+			frame_loop = 0;
+		}
+		glGenBuffers(1, vboHandle); // create two VBO handles, one position, one color handle 
+		glBindBuffer(GL_ARRAY_BUFFER, vboHandle[0]); // bind the first handle 
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)*16, verts, GL_STATIC_DRAW); // allocate space and copy the 
+		// position data over 
+		glBindBuffer(GL_ARRAY_BUFFER, 0); // clean up 
+		glGenBuffers(1, indexVBO); 
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVBO[0]); 
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLubyte)*indeices[frame_loop].size, indeices[frame_loop].order, GL_STATIC_DRAW); // load the 
+		// index data 
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0); // clean up 
+	}
 }
