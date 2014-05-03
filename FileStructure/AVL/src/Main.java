41a362c3dd1574ce724eedf9caf742c2e24650aa
Francis Kim <kims_0121@naver.com>
2014-05-03 12:24:38 +0900

    Computer Network HW 2완료

---
+++ FileStructure/AVL/src/Main.java
@@ -0,0 +1,15 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		logger.debug("DO");
+	}
+
+}
